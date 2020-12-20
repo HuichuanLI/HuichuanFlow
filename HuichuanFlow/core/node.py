af41db34bb8111f6007d568ff22e0cfac12a2738
huichuan <lhc14124908@163.com>
2020-12-20 21:49:40 +0800

    add node-realization

--- HuichuanFlow/core/node.py
+++ HuichuanFlow/core/node.py
@@ -3,3 +3,113 @@
 # @Author : huichuan LI
 # @File : node.py
 # @Software: PyCharm
+
+import abc
+
+import numpy as np
+
+
+class Node(object):
+    """
+    计算图节点类基类
+    """
+
+    def __init__(self, *parents, **kargs):
+        # 计算图对象，默认为全局对象default_graph
+        self.kargs = kargs
+        self.graph = kargs.get('graph', default_graph)
+        self.need_save = kargs.get('need_save', True)
+        self.gen_node_name(**kargs)
+
+        self.parents = list(parents)  # 父节点列表
+        self.children = []  # 子节点列表
+        self.value = None  # 本节点的值
+        self.jacobi = None  # 结果节点对本节点的雅可比矩阵
+
+        # 将本节点添加到父节点的子节点列表中
+        for parent in self.parents:
+            parent.children.append(self)
+
+    def get_parents(self):
+        """
+        获取本节点的父节点
+        """
+        return self.parents
+
+    def get_children(self):
+        """
+        获取本节点的子节点
+        """
+        return self.children
+
+    def forward(self):
+        """
+        前向传播计算本节点的值，若父节点的值未被计算，则递归调用父节点的forward方法
+        """
+        for node in self.parents:
+            if node.value is None:
+                node.forward()
+
+        self.compute()
+
+    def gen_node_name(self, **kargs):
+        """
+        生成节点名称，如果用户不指定，则根据节点类型生成类似于"MatMul:3"的节点名，
+        如果指定了name_scope，则生成类似"Hidden/MatMul:3"的节点名
+        """
+        self.name = kargs.get('name', '{}:{}'.format(
+            self.__class__.__name__, self.graph.node_count()))
+        if self.graph.name_scope:
+            self.name = '{}/{}'.format(self.graph.name_scope, self.name)
+
+    @abc.abstractmethod
+    def compute(self):
+        """
+        抽象方法，根据父节点的值计算本节点的值
+        """
+
+    @abc.abstractmethod
+    def get_jacobi(self, parent):
+        """
+        抽象方法，计算本节点对某个父节点的雅可比矩阵
+        """
+
+    def backward(self, result):
+        """
+        反向传播，计算结果节点对本节点的雅可比矩阵
+        """
+        if self.jacobi is None:
+            if self is result:
+                self.jacobi = np.mat(np.eye(self.dimension()))
+            else:
+                self.jacobi = np.mat(
+                    np.zeros((result.dimension(), self.dimension())))
+
+                for child in self.get_children():
+                    if child.value is not None:
+                        self.jacobi += child.backward(result) * child.get_jacobi(self)
+
+        return self.jacobi
+
+    def clear_jacobi(self):
+        """
+        清空结果节点对本节点的雅可比矩阵
+        """
+        self.jacobi = None
+
+    def shape(self):
+        """
+        返回本节点的值作为矩阵的形状：（行数，列数）
+        """
+        return self.value.shape
+
+    def reset_value(self, recursive=True):
+        """
+        重置本节点的值，并递归重置本节点的下游节点的值
+        """
+
+        self.value = None
+
+        if recursive:
+            for child in self.children:
+                child.reset_value()
