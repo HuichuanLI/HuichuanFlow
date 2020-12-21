56067aee3fe003b0044ded82a6fd623ec72fcd3f
huichuan <lhc14124908@163.com>
2020-12-21 19:36:10 +0800

    add

--- HuichuanFlow/ops/loss.py
+++ HuichuanFlow/ops/loss.py
@@ -8,6 +8,7 @@
 import numpy as np
 
 from ..core import node
+from ..ops import SoftMax
 
 
 class LossFunction(node.Node):
@@ -39,6 +40,7 @@ class LogLoss(LossFunction):
     """
     对数损失函数
     """
+
     def compute(self):
         assert len(self.parents) == 1
 
@@ -51,3 +53,22 @@ class LogLoss(LossFunction):
         diag = -1 / (1 + np.power(np.e, np.where(x > 1e2, 1e2, x)))
 
         return np.diag(diag.ravel())
+
+
+class CrossEntropyWithSoftMax(LossFunction):
+    """
+    对第一个父节点施加SoftMax之后，再以第二个父节点为标签One-Hot向量计算交叉熵
+    """
+
+    def compute(self):
+        prob = SoftMax.softmax(self.parents[0].value)
+        self.value = np.mat(
+            -np.sum(np.multiply(self.parents[1].value, np.log(prob + 1e-10))))
+
+    def get_jacobi(self, parent):
+        # 这里存在重复计算，但为了代码清晰简洁，舍弃进一步优化
+        prob = SoftMax.softmax(self.parents[0].value)
+        if parent is self.parents[0]:
+            return (prob - self.parents[1].value).T
+        else:
+            return (-np.log(prob)).T
